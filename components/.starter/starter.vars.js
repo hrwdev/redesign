3a83bf988540ec85ca7df8f69c457a3ba75eb797
Les Lim <liml@hrw.org>
2024-04-04 23:13:55 -0500

    Simplify starter component

--- components/.starter/starter.vars.js
+++ components/.starter/starter.vars.js
@@ -4,7 +4,4 @@ vars.starter = {
     title: 'My title',
     image: '<img width="484" height="436" src="https://www.hrw.org/sites/default/files/media_2022/11/202210hr_careers_header_0.jpg" alt="Human rights protest" />',
   },
-  es: {
-    title: 'Mi título',
-  },
 };
