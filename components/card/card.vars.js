042d2f381de5d5938353f35c38bcac9ee6e61348
Les Lim <liml@hrw.org>
2024-04-07 22:25:23 -0500

    Added card component

---
+++ components/card/card.vars.js
@@ -0,0 +1,22 @@
+vars.card = {
+  default: {
+    style: 'stacked',
+    size: 'large',
+    url: '#',
+    image: '<img width="4000" height="2666" src="https://www.hrw.org/sites/default/files/styles/embed_xxl/public/media_2024/04/202403africa_Rwanda_CorinnePhotoTutsi.jpg?itok=zQ021UuA" alt="House with the word &quot;Tutsi&quot; written in chalk on the outside wall" />',
+    date: 'April 2, 2024',
+    type: 'News Release',
+    heading_level: 'h2',
+    title: 'Rwanda: Genocide Archives Released',
+    flair: 'Most Read',
+    teaser: 'Human Rights Watch is releasing a series of archives highlighting the extraordinary efforts of human rights defenders in Rwanda and abroad, to warn about the planned 1994 genocide and attempt to stop the killings. The documents painfully illustrate leading international actors’ refusal to acknowledge the slaughter of more than half a million people and act to end it.',
+  },
+  stacked: {
+    style: 'stacked',
+  },
+  sidebyside: {
+    style: 'sidebyside',
+    size: 'small',
+    teaser: false,
+  }
+};
