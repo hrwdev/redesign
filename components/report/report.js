bb3e530b0e7425c9dad6f229d8133622b03589d1
Les Lim <liml@hrw.org>
2024-04-12 09:53:07 -0500

    Add report component

---
+++ components/report/report.js
@@ -0,0 +1,5 @@
+document.addEventListener('twigRendered', (ev) => {
+  document.querySelectorAll('.report').forEach((element) => {
+    // component JS code goes here
+  });
+});
---
+++ components/report/report.vars.js
@@ -0,0 +1,11 @@
+vars.report = {
+  default: {
+    title: 'Neglected in the Jungle',
+    subtitle: 'Inadequate Protection and Assistance for Migrants and Asylum Seekers Crossing the Darién Gap',
+    url: 'https://www.hrw.org/report/2024/04/03/neglected-jungle/inadequate-protection-and-assistance-migrants-and-asylum-seekers',
+    image: '<img width="639" height="828" src="https://www.hrw.org/sites/default/files/styles/720w/public/media_2024/03/202404americas_panama_dariengap_reportcover.jpg" alt="Human rights protest" />',
+    teaser: 'Human Rights Watch identified specific shortcomings in Colombia’s and Panama’s efforts to protect and assist people – including those at higher risk, such as unaccompanied children – as well as to investigate abuses against them.',
+    date: 'April 3, 2024',
+    pagecount: '110 pages',
+  },
+};
