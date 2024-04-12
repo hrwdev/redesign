a7f97cf1d480b34338c74cfe11f8e641ccfe7ae2
Les Lim <liml@hrw.org>
2024-04-12 10:11:51 -0500

    add reports component

---
+++ components/reports/reports.js
@@ -0,0 +1,5 @@
+document.addEventListener('twigRendered', (ev) => {
+  document.querySelectorAll('.reports').forEach((element) => {
+    // component JS code goes here
+  });
+});
---
+++ components/reports/reports.vars.js
@@ -0,0 +1,8 @@
+vars.reports = {
+  default: {
+    title: 'Reports',
+    description: '<p>Our investigative team takes a deep dive on some of today’s most pressing human rights issues. From women’s rights and censorship to health crises and free speech, Human Rights Watch’s team investigates global issues and their impact.</p>',
+    link_url: '#',
+    link_text: 'See more reports',
+  },
+};
