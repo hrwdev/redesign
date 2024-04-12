fc7cac45330a4d7b64b1b288d49674b010ff7b8a
Les Lim <liml@hrw.org>
2024-04-12 10:56:28 -0500

    Added hero_brand component

---
+++ components/hero_brand/hero_brand.js
@@ -0,0 +1,5 @@
+document.addEventListener('twigRendered', (ev) => {
+  document.querySelectorAll('.hero_brand').forEach((element) => {
+    // component JS code goes here
+  });
+});
---
+++ components/hero_brand/hero_brand.vars.js
@@ -0,0 +1,15 @@
+vars.hero_brand = {
+  default: {
+    image: "<img src='https://aaf1a18515da0e792f78-c27fdabe952dfc357fe25ebf5c8897ee.ssl.cf5.rackcdn.com/1908/monthly-donation-form-hero-flipped.jpg?v=1663603656000' width='1512' height='800'/>",
+    eyebrow: "",
+    headline: "Defending the Rights of People Worldwide",
+    body: "<p>Civilians living in warzones. Racial and religious minorities. LGBTQI+ people. We protect the freedom, safety and dignity of everyone. Donate today and join us in defending human rights in your country and around the globe.</p>",
+    ctas: [
+      {url: "#", text: "Primary CTA", type: "primary"},
+      {url: "#", text: "Secondary CTA", type: "secondary"},
+    ],
+  },
+  option2: {
+    body: "<p>Civilians living in warzones. Racial and religious minorities. LGBTQI+ people. We work where people’s safety and freedom is at risk. Donate now to defend human rights and save lives.</p>",
+  }
+};
