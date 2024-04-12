462637eb15658625312786ebd479c1dd151ee00a
Les Lim <liml@hrw.org>
2024-04-12 15:18:27 -0500

    hero_brand: update headline

--- components/hero_brand/hero_brand.vars.js
+++ components/hero_brand/hero_brand.vars.js
@@ -2,7 +2,7 @@ vars.hero_brand = {
   default: {
     image: "<img src='https://aaf1a18515da0e792f78-c27fdabe952dfc357fe25ebf5c8897ee.ssl.cf5.rackcdn.com/1908/monthly-donation-form-hero-flipped.jpg?v=1663603656000' width='1512' height='800'/>",
     eyebrow: "",
-    headline: "Defending the Rights of People Worldwide",
+    headline: "Defend Rights. Secure Justice.",
     body: "<p>Civilians in warzones. Racial minorities. LGBT+ people. We protect the freedom, safety, and dignity of everyone. Donate now to defend human rights and save lives.</p>",
     ctas: [
       {url: "#", text: "Donate", type: "primary"},
