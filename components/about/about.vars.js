dfb756d8e0fd7223f709a0238324737a48bc13cd
Les Lim <liml@hrw.org>
2024-04-12 14:37:26 -0500

    Update hero_brand, about default language

--- components/about/about.vars.js
+++ components/about/about.vars.js
@@ -1,10 +1,10 @@
 vars.about = {
   default: {
-    headline: 'Defend Rights. Secure Justice.',
+    headline: 'How We Work',
     image: '<img width="484" height="484" src="https://www.hrw.org/sites/default/files/media_2022/11/202210hr_careers_header_0.jpg" alt="Human rights protest" />',
-    body: '<p><strong>Investigate:</strong> Our researchers investigate wrongdoing, from systemic racism to war crimes.</p>' +
-      '<p><strong>Expose:</strong> We publicize our findings and mobilize the public, pressuring people in power to make a difference.</p>' +
-      '<p><strong>Change:</strong> Our advocates meet with governments, the United Nations and other groups to see that policy is changed, laws are enforced and justice is served.</p>',
+    body: '<p><strong>Investigate:</strong> Our researchers investigate wrongdoings globally.</p>' +
+      '<p><strong>Expose:</strong> We publicize our findings and mobilize the public.</p>' +
+      '<p><strong>Change:</strong> Our advocates meet with governments and other groups to change policy, enforce laws, and deliver justice.</p>',
     link: {type: 'primary', url: '#', text: "Our Impact"},
   },
   option2: {
