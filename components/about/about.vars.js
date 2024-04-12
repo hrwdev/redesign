d29fa42c6d512d3d2aba3eaa6a5459adb362ff0e
Les Lim <liml@hrw.org>
2024-04-12 09:53:29 -0500

    Update 'about' vars; add util-block-spacing class

--- components/about/about.vars.js
+++ components/about/about.vars.js
@@ -1,8 +1,16 @@
 vars.about = {
   default: {
-    headline: 'Investigate. Expose. Change.',
+    headline: 'Defend Rights. Secure Justice.',
     image: '<img width="484" height="484" src="https://www.hrw.org/sites/default/files/media_2022/11/202210hr_careers_header_0.jpg" alt="Human rights protest" />',
-    body: '<p>We are an independent coalition of journalists, lawyers and country experts fighting to protect the freedom, safety and dignity of the most vulnerable—from minorities and civilians in wartime to refugees to children in need.</p>',
+    body: '<p><strong>Investigate:</strong> Our researchers investigate wrongdoing, from systemic racism to war crimes.</p>' +
+      '<p><strong>Expose:</strong> We publicize our findings and mobilize the public, pressuring people in power to make a difference.</p>' +
+      '<p><strong>Change:</strong> Our advocates meet with governments, the United Nations and other groups to see that policy is changed, laws are enforced and justice is served.</p>',
     link: {type: 'primary', url: '#', text: "Our Impact"},
   },
+  option2: {
+    headline: 'We stay with issues until we get results',
+    body: '<p><strong>Investigate:</strong> Our researchers investigate wrongdoing, from systemic racism to war crimes.</p>' +
+      '<p><strong>Expose:</strong> We publicize our findings, giving voice to victims and mobilizing the public.</p>' +
+      '<p><strong>Change:</strong> Our advocates meet with governments, the United Nations and other groups who can hold people accountable for their crimes and make justice happen.</p>',
+  }
 };
