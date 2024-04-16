be949baa5a12baab5a11f5dd371147a8b04c42c6
Les Lim <liml@hrw.org>
2024-04-16 12:43:50 -0500

    report: adjust touch vs hover behaviors/styles

--- components/report/report.js
+++ components/report/report.js
@@ -1,8 +1,12 @@
 document.addEventListener('twigRendered', (ev) => {
   document.querySelectorAll('.report').forEach((element) => {
     // Prevent touch devices from simulating a mouseover event on touch.
-    element.addEventListener('touchend', (event) => {
-      event.stopPropagation();
-    })
+    const button = element.querySelector('.report__button');
+    button.addEventListener('click', (event) => {
+      (button.getAttribute('aria-expanded') === 'false')
+        ? button.setAttribute('aria-expanded', 'true')
+        : button.setAttribute('aria-expanded', 'false')
+      ;
+    });
   });
 });
