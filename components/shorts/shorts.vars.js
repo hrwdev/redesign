fb71753e6fbdd3b87db508543c8754efcdca9956
Les Lim <liml@hrw.org>
2024-04-21 23:45:17 -0500

    shorts: add links to social

--- components/shorts/shorts.vars.js
+++ components/shorts/shorts.vars.js
@@ -1,7 +1,7 @@
 vars.shorts = {
   default: {
     title: 'Quick Takes',
-    description: '<p>Short videos from HRW</p>',
+    description: '<p>Short videos from HRW. Find more on <a href="#">X</a> | <a href="#">Facebook</a> | <a href="#">LinkedIn</a> | <a href="#">TikTok</a> | <a href="#">Instagram</a> | <a href="#">YouTube</a></p>',
     options: '{"perPage": 1, "gap": 20, "padding": 20, "autoWidth": true, "breakpoints": {"480": {"perPage": 2}}}',
     items: [
       'foo',
