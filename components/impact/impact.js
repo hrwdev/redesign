83776c450d65ab30388bb6898e5147e8d794ca69
Les Lim <liml@hrw.org>
2024-04-24 15:40:14 -0500

    Added impact component

---
+++ components/impact/impact.js
@@ -0,0 +1,5 @@
+document.addEventListener('twigRendered', (ev) => {
+  document.querySelectorAll('.impact').forEach((element) => {
+    // component JS code goes here
+  });
+});
---
+++ components/impact/impact.vars.js
@@ -0,0 +1,17 @@
+vars.impact = {
+  default: {
+    title: 'Nearly 50 years of impact',
+    body: '<p>We’ve helped put war criminals behind bars, XYZ, and received a Nobel Peace Prize for our work banning landmines.</p>',
+    link_url: 'https://www.hrw.org/impact',
+    link_text: 'See what else we\'ve achieved',
+    options: '{"perPage": 1, "gap": 20, "padding": 20, "breakpoints": {"768": {"destroy": true}}}',
+    items: [
+      'foo',
+      'bar',
+      'baz',
+      'quux',
+      'flarp',
+      'zlok'
+    ],
+  },
+};
