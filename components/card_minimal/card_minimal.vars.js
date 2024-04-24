eb762e6fa695e55d722897cd9fc0df756f919c1a
Les Lim <liml@hrw.org>
2024-04-24 16:23:14 -0500

    card_minimal: add support for optional image

--- components/card_minimal/card_minimal.vars.js
+++ components/card_minimal/card_minimal.vars.js
@@ -19,6 +19,51 @@ vars.card_minimal = {
     title: 'Letter to the NEC Chairman Regarding Alleged Intimidation and Vote Buying in February 25, 2024 Senate Election',
     date: 'April 3, 2024',
   },
+  en4: {
+    url: '#',
+    title: 'EU Misses Opportunity on Frontex Transparency, Accountability',
+    date: 'April 24, 2024',
+  },
+  en5: {
+    url: '#',
+    title: 'Indian Authorities Stop Australian Journalist from Covering Elections',
+    date: 'April 24, 2024',
+  },
+  en6: {
+    url: '#',
+    title: 'Japan’s Transgender Law Revisions Should Be Grounded in Autonomy',
+    date: 'April 24, 2024',
+  },
+  en7: {
+    url: '#',
+    title: 'Shining a Light on North Korea’s Human Rights Crisis',
+    date: 'April 23, 2024',
+  },
+  en8: {
+    url: '#',
+    title: 'Can New African Union Genocide Envoy Curb Atrocities in Africa?',
+    date: 'April 23, 2024',
+  },
+  en9: {
+    url: '#',
+    title: 'The UK Again Attempts to Bend Truth on Rwanda',
+    date: 'April 23, 2024',
+  },
+  en10: {
+    url: '#',
+    title: 'Angola: Proposed Security Law Threatens Rights',
+    date: 'April 23, 2024',
+  },
+  en11: {
+    url: '#',
+    title: 'Australia: Withdraw Punitive Migration Bill',
+    date: 'April 22, 2024',
+  },
+  en12: {
+    url: '#',
+    title: 'On Earth Day, a Homage to a Beloved Forest Defender',
+    date: 'April 22, 2024',
+  },
   ar: {
     url: 'https://www.hrw.org/ar/news/2024/04/17/west-bank-israel-responsible-rising-settler-violence',
     title: 'الضفة الغربية: إسرائيل مسؤولة عن تصاعد عنف المستوطنين',
@@ -38,5 +83,41 @@ vars.card_minimal = {
     url: 'https://www.hrw.org/ar/news/2024/04/04/bulgaria-alleged-beating-detained-saudi-activist',
     title: 'بلغاريا: ادعاءات ضرب ناشط سعودي محتجز',
     date: 'أبريل/نيسان 4, 2024',
+  },
+  image1: {
+    url: '#',
+    image: '<img src="https://www.hrw.org/sites/default/files/styles/16x9_large/public/media_2024/01/202401eca_uk_rwandaplan_protest.jpg?h=7909ff12&amp;itok=PbpuMLEX" alt="Protesters hold a banner on a red double decker bus " loading="lazy" class="card__image unloaded w-auto" width="400" height="225">',
+    title: 'UK’s Harmful Rwanda Bill to Become Law',
+    date: 'April 23, 2024',
+  },
+  image2: {
+    url: '#',
+    image: '<img src="https://www.hrw.org/sites/default/files/styles/16x9_large/public/media_2024/04/202404lgbt_ame_dominica_Roseau_and_high_court_building.jpg?h=f52cdf31&amp;itok=kQTRQLcB" alt="Dominica’s High Court of Justice in the capital Roseau,&nbsp; pictured with other commercial and government buildings, January 9, 2023." loading="lazy" class="card__image unloaded w-auto" width="400" height="225">',
+    title: 'Dominica High Court Decriminalizes Same-Sex Conduct',
+    date: 'April 24, 2024',
+  },
+  image3: {
+    url: '#',
+    image: '<img src="https://www.hrw.org/sites/default/files/styles/16x9_large/public/media_2024/04/202404asia_philippines_Rene_Saguisag.jpg?h=ee90139f&amp;itok=airi2d56" alt="Human rights lawyer and former senator Rene Saguisag holds a copy of a book about the martial law period in the Philippines while describing his ordeal in detention, Manila, September 26, 2018. " loading="lazy" class="card__image unloaded w-auto" width="400" height="225">',
+    title: 'Honoring a Philippine Human Rights Icon',
+    date: 'April 24, 2024',
+  },
+  image4: {
+    url: '#',
+    image: '<img src="https://www.hrw.org/sites/default/files/styles/16x9_large/public/media_2024/04/202404arms_ukraine_statue.jpg?h=c6980913&amp;itok=MAPLSSda" alt="A statue stands amidst the ruins of a museum " loading="lazy" class="card__image unloaded w-auto" width="400" height="225">',
+    title: 'Explosive Weapons’ Dire Impact on Cultural Heritage',
+    date: 'April 18, 2024',
+  },
+  image5: {
+    url: '#',
+    image: '<img src="https://www.hrw.org/sites/default/files/styles/16x9_large/public/media_2024/04/202404ame_peru_la_oroya.jpg?h=43e41a76&amp;itok=O9pKC7Hd" alt="The Metalurgia Business Peru metallurgical complex in the city of La Oroya, in the department of Junín, east of Lima, November 9, 2022.&nbsp;" loading="lazy" class="card__image unloaded w-auto" width="400" height="225">',
+    title: 'Landmark Court Ruling Upholds Right to Healthy Environment',
+    date: 'April 18, 2024',
+  },
+  image6: {
+    url: '#',
+    image: '<img src="https://www.hrw.org/sites/default/files/styles/16x9_large/public/media_2024/04/202404africa_sudan_oneyearatrocities.jpg?h=5021cb65&amp;itok=oI1-VLjb" alt="A destroyed medical storage facility in Nyala, the capital of South Darfur province, Sudan, May 2, 2023." loading="lazy" class="card__image unloaded w-auto" width="400" height="225">',
+    title: 'Sudan: One Year of Atrocities Requires New Global Approach',
+    date: 'April 12, 2024',
   },
 };
