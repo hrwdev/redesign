69003d5af4d259b9570b58a10cd997c836afdb51
Les Lim <liml@hrw.org>
2024-04-25 12:51:52 -0500

    modal_disclosure: adjust video iframe containment

--- components/modal_disclosure/modal_disclosure.vars.js
+++ components/modal_disclosure/modal_disclosure.vars.js
@@ -2,16 +2,16 @@ vars.modal_disclosure = {
   default: {
     id: 'hero-video',
     title: 'What is Human Rights Watch?',
-    body: '<iframe style="width: 70vw; aspect-ratio: 16 / 9;" src="https://www.youtube.com/embed/LudmeeQPCMs?si=i3QFWcFZ-I7A03Iv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-    footer: '<a class="link-button button-primary" href="#">Learn more</a>'
+    body: '<iframe class="modal-disclosure__iframe" src="https://www.youtube.com/embed/LudmeeQPCMs?si=i3QFWcFZ-I7A03Iv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    footer: '<a class="link-button button-primary button-text" href="#">Learn more</a>'
   },
   es: {
     id: 'hero-video-es',
     title: '¿Qué es Human Rights Watch?',
-    footer: '<a class="link-button button-primary" href="#">Aprende más</a>'
+    footer: '<a class="link-button button-primary button-text" href="#">Aprende más</a>'
   },
   ar: {
     title: 'ما هي هيومن رايتس ووتش؟',
-    footer: '<a class="link-button button-primary" href="#">يتعلم أكثر</a>'
+    footer: '<a class="link-button button-primary button-text" href="#">يتعلم أكثر</a>'
   }
 };
